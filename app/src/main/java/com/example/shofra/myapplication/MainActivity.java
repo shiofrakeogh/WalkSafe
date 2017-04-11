457ddbc783f409bad7478c29ee528ec3490fe6f8
Eva Finn <15172899@studentmail.ul.ie>
2017-04-11 22:16:20 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/shofra/myapplication/MainActivity.java
+++ app/src/main/java/com/example/shofra/myapplication/MainActivity.java
@@ -1,19 +1,37 @@
-package com.example.shofra.myapplication;
-import android.support.v7.app.AppCompatActivity;
+package eva.walksafe;
+
+import android.app.AlarmManager;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.content.Intent;
+
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity
 {
+    Intent intent;
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         final Button button = (Button) findViewById(R.id.button);
         button.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -24,12 +42,44 @@ public class MainActivity extends AppCompatActivity
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
                     button.setBackgroundResource(R.drawable.button);
                     //Start a timer
-                    Toast.makeText(getApplicationContext(), "Enter your pin", Toast.LENGTH_LONG).show();
+                    //Toast.makeText(getApplicationContext(), "Enter your pin", Toast.LENGTH_LONG).show();
+                    start();
+                    PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+                    ((AlarmManager) getSystemService(ALARM_SERVICE)).set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 1000, pendingIntent);
                 }
                 return true;
             }
         });
     }
+
+    public void start(){
+        intent = new Intent(this,PinKeypadActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+
+   @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+    }
+
+     @Override
+     public boolean onOptionsItemSelected(MenuItem item) {
+     // Handle action bar item clicks here. The action bar will
+     // automatically handle clicks on the Home/Up button, so long
+     // as you specify a parent activity in AndroidManifest.xml.
+     int id = item.getItemId();
+
+     //noinspection SimplifiableIfStatement
+     if (id == R.id.action_settings) {
+         Intent intent = new Intent(this,SettingsActivity.class);
+         startActivity(intent);
+         //return true;
+     }
+
+     return super.onOptionsItemSelected(item);
+     }
 }
-
-
